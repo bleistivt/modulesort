7419d15bb023f1f0da12e18e16fa90515f2f3210
bleistivt <bleistivt@users.noreply.github.com>
2018-05-04 11:55:08 +0200

    update to new dashboard style

--- views/modulesort.php
+++ views/modulesort.php
@@ -1,28 +1,37 @@
 <?php if (!defined('APPLICATION')) exit();
 
-$item = '<li data-name="%1$s"><div><table><tr>'
-        .'<td class="Name">%1$s</td>'
-        .'<td class="Buttons"><button class="SmallButton RemoveModule">'.t('Remove').'</button></td>'
-        .'</tr></table></div></li>';
+$item = '<li data-name="%1$s"><div class="plank" style="cursor:grab;">'
+        .'<div class="plank-title">%1$s</div>'
+        .'<div class="plank-options"><button class="btn btn-secondary modulesort-remove">'.t('Remove').'</button></div>'
+        .'</div></li>';
 
-echo wrap($this->data('Title'), 'h1');
+echo heading($this->data('Title'));
 
-echo '<div class="Info">',
+echo '<div class="padded">',
     wrap(t('Drag & drop modules to sort them. Removing modules from the list does not make them disappear, it just removes them from the defined sort order.'), 'p'),
-    wrap(t('Your settings are saved automatically.').'<br>'.anchor(t('Restore defaults'), 'settings/modulesort/reset', 'Hijack'), 'p'),
+    wrap(t('Your settings are saved automatically.').'<br><br>'.anchor(t('Restore defaults'), 'settings/modulesort/reset', 'Hijack btn btn-primary'), 'p'),
     '</div>';
 
 foreach (array_keys(c('EnabledApplications')) as $app) {
-    echo '<form class="AddModule" data-group="'.$app.'" style="float:right;padding:6px;">',
-        '<input type="text" class="InputBox" placeholder="'.t('Add module...').'">',
-        '<button class="Button">'.t('Add').'</button>',
-        '</form>',
-        wrap($app, 'h1'),
-        '<ol class="Sortable Sort-'.$app.'" data-appname="'.$app.'">';
+    $app = htmlspecialchars($app);
+    ?>
+    <div class="header-block">
+        <h2><?php echo $app; ?></h2>
+        <form class="modulesort-add" data-group="<?php echo $app; ?>">
+            <div class="input-wrap input-wrap-multiple">
+                <input type="text" class="form-control" placeholder="<?php echo t('Add module...'); ?>">
+                <button class="btn btn-primary"><?php echo t('Add'); ?></button>
+            </div>
+        </form>
+    </div>
+    <ol class="modulesort sort-<?php echo $app; ?> nestable-list padded-top" data-appname="<?php echo $app; ?>">
+    <?php
     foreach (c('Modules.'.$app.'.Panel', []) as $module) {
         echo sprintf($item, $module);
     }
-    echo '</ol>';
+    ?>
+    </ol>
+    <?php
 }
 
-echo '<div class="Hidden Dummy">'.sprintf($item, '').'</div>';
+echo '<div class="hidden modulesort-dummy">'.sprintf($item, '').'</div>';
