6c70f554ceb6743fed86812493fa477a4e69c46f
bleistivt <bleistivt@users.noreply.github.com>
2021-03-22 02:10:24 +0100

    ES6

--- js/modulesort.js
+++ js/modulesort.js
@@ -1,32 +1,29 @@
 /*global gdn, jQuery*/
 
-jQuery(function ($) {
-    'use strict';
+jQuery(($) => {
+    const lists = $('ol.modulesort');
 
-    var lists = $('ol.modulesort'),
-        update = function () {
-            var modules = {};
+    const update = () => {
+        const modules = {};
 
-            lists.each(function (ignore, list) {
-                modules[$(list).data('appname')] = {
-                    Panel: $('li', list).map(function (ignore, item) {
-                        return $(item).data('name');
-                    }).get()
-                };
-            });
+        lists.each((ignore, list) => {
+            modules[$(list).data('appname')] = {
+                Panel: $('li', list).map((ignore, item) => $(item).data('name')).get()
+            };
+        });
 
-            $.post(gdn.url('settings/modulesort.json'), {
-                Modules: JSON.stringify(modules),
-                TransientKey: gdn.definition('TransientKey')
-            });
-        };
+        $.post(gdn.url('settings/modulesort.json'), {
+            Modules: JSON.stringify(modules),
+            TransientKey: gdn.definition('TransientKey')
+        });
+    };
 
     lists.sortable({forcePlaceholderSize: true}).on('sortupdate', update);
 
-    $('.modulesort-add').submit(function (e) {
-        var $this = $(e.currentTarget),
-            input = $this.find('.form-control'),
-            list = $('ol.sort-' + $this.data('group'));
+    $('.modulesort-add').submit(({currentTarget}) => {
+        const $this = $(currentTarget);
+        const input = $this.find('.form-control');
+        const list = $('ol.sort-' + $this.data('group'));
 
         $('.modulesort-dummy li')
             .clone().data('name', input.val())
@@ -40,9 +37,8 @@ jQuery(function ($) {
         return false;
     });
 
-    lists.on('click', '.modulesort-remove', function (e) {
-        $(e.currentTarget).closest('li').remove();
+    lists.on('click', '.modulesort-remove', ({currentTarget}) => {
+        $(currentTarget).closest('li').remove();
         update();
     });
-
 });
